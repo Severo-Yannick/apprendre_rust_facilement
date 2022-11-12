b0915d75c3abcc51e049a0a524aa2af19c56f468
Severo-Yannick <severoyannick@gmail.com>
2022-11-12 10:24:43 +0100

    comments module

---
+++ exemples/src/_1_comments_module.rs
@@ -0,0 +1,9 @@
+pub fn log_comments() {
+  // Commentaire sur une linge
+  /* Commentaire multilignes
+    Lorem ipsum is placeholder text
+    commonly used in the graphic,
+    print, and publishing industries for previewing layouts and visual mockups
+  */
+  println!("Ici j'utilise des commentaires");
+}
---
+++ exemples/src/main.rs
@@ -0,0 +1,8 @@
+/* Pour test il faut decommenter le module ici et l'appel de la fonction dans main()
+Puis lancer la commande: cargo run */
+
+// mod _1_comments_module;
+fn main() {
+    // Penses à décommenter l'appel de la fonction
+    // _1_comments_module::log_comments();
+}
